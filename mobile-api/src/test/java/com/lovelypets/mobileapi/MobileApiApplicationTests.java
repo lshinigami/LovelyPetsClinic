7682988aab2383783b915bf0a178a1c671e5910b
Shinigami <funteshka5@gmail.com>
2026-05-15 22:12:41 +0500

    Split monolith application into four modules: core, crm, domain, and mobile-api.

---
+++ mobile-api/src/test/java/com/lovelypets/mobileapi/MobileApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lovelypets.mobileapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
