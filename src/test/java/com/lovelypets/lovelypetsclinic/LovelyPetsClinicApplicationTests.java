7134705d35558342ce663c2a667e60c6f7c02aa7
Shinigami <funteshka5@gmail.com>
2026-05-15 14:31:42 +0500

    init

---
+++ src/test/java/com/lovelypets/lovelypetsclinic/LovelyPetsClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lovelypets.lovelypetsclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovelyPetsClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
